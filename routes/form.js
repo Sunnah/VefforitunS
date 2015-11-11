3f0ade713cd4f301371c767d0fe887dfbf8f5db6
Gunnsteinn Aron Skúlason <gunnsteinnskula@gmail.com>
2015-11-11 17:28:00 +0000

    Lagadi utlit a sida.jade, lagadi css og matchadi vid form.jade

--- routes/form.js
+++ routes/form.js
@@ -16,7 +16,14 @@ router.post('/', function(req, res) {
 		subheader:'',
 		adresse:'',
 		val:'',
-		subheader:''
+		subheader:'',
+		description: '',
+		hobbie1:'',
+		hobbieD1:'',
+		hobbieP1:'',
+		hobbie2:'',
+		hobbieD2:'',
+		hobbieP2:'',
 	};
 	renderData.errors=villumelding(req.body);
 	if(renderData.errors.length!==0)
@@ -28,6 +35,13 @@ function putIn(renderData, data){
 	renderData.name=data.name;
 	renderData.pf=data.pf;
 	renderData.subheader=data.subheader;
+	renderData.description=data.description;
+	renderData.hobbie1=data.hobbie1;
+	renderData.hobbieD1=data.hobbieD1;
+	renderData.hobbieP1=data.hobbieD1;
+	renderData.hobbie2=data.hobbie2;
+	renderData.hobbieD2=data.hobbieD2;
+	renderData.hobbieP2=data.hobbieP2;
 	renderData.adresse=data.adresse;
 	renderData.pn=data.pn;
 	renderData.subheader=data.subheader;
